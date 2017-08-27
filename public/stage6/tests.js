2a52e73c638e9671a333c9e7c784a4ea8191eb5f
Tomoko Senju <xxpapapandaxx@Tomoko-MBP.local>
2017-08-27 18:03:54 +0900

    stage6 : Done

--- public/stage6/tests.js
+++ public/stage6/tests.js
@@ -5,7 +5,7 @@ describe('ステージ6（意図通りにモジュールを書ける）', functi
     // ボタンはサービスです。
     // 自由に使ってください。
 
-    var qualityOfYourAppliation = undefined;
+    var qualityOfYourAppliation = true;
 
     expect(qualityOfYourAppliation).to.be.ok;
   });
